2390e37def858807ee049805761afcce455882ae
Charley McGowan and Sheila Stephen <sheila@>
2018-11-14 16:49:32 -0800

    End of day commit, need diagonal attack to work

--- QueenAttack.Tests/ModelTests/QueenAttackTest.cs
+++ QueenAttack.Tests/ModelTests/QueenAttackTest.cs
@@ -1,16 +1,24 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
-namespace QueenAttack.Tests
+namespace QueenAttackGame.Tests
 {
   [TestClass]
   public class QueenAttackTest
   {
     [TestMethod]
-        public void checkCoordinates_test_true()
+        public void checkXCoordinates_test_true()
         {
-          QueenAttack testQueenAttack = new QueenAttack();
-          Assert.AreEqual(true, testQueenAttack.checkCoordinates());
+          int[] queenCoordtest = new int[] {1, 2};
+          int[] playerCoordtest = new int[] {1, 4};
+          Assert.AreEqual(true, QueenAttack.checkCoordinates(queenCoordtest,playerCoordtest));
+        }
+    [TestMethod]
+        public void checkYCoordinates_test_true()
+        {
+          int[] queenCoordtest = new int[] {3, 2};
+          int[] playerCoordtest = new int[] {4, 2};
+          Assert.AreEqual(true, QueenAttack.checkCoordinates(queenCoordtest,playerCoordtest));
         }
   }
 }
